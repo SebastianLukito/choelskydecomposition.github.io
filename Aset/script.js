6f2eec7a23d401acd841bf57377f4f518117f166
SebastianLukito <113852181+SebastianLukito@users.noreply.github.com>
2024-01-27 20:08:55 +0700

    Add files via upload

--- Aset/script.js
+++ Aset/script.js
@@ -3,7 +3,7 @@ function generateMatrix() {
   const columns = parseInt(document.getElementById('columns').value);
 
   if (isNaN(rows) || isNaN(columns) || rows <= 0 || columns <= 0) {
-      alert('Please enter valid rows and columns.');
+      alert('Tolong masukkan baris dan kolom yang valid.');
       return;
   }
 
@@ -38,7 +38,7 @@ function createMatrixTable(rows, columns, matrix = []) {
 function generateSymmetricPositiveDefiniteMatrix() {
   const size = parseInt(document.getElementById('rows').value);
   if (isNaN(size) || size <= 0) {
-      alert('Please enter a valid matrix size.');
+      alert('Tolong masukkan ukuran matriks yang valid.');
       return;
   }
 
@@ -90,7 +90,7 @@ function generateSymmetricPositiveDefiniteMatrix() {
   
     // Validate the matrix for Cholesky decomposition
     if (!isSymmetricPositiveDefinite(matrix)) {
-      alert('Please enter a valid symmetric positive definite matrix.');
+      alert('Matriks yang dimasukkan bukan matriks simetris positif definit.');
       return;
     }
   
@@ -204,3 +204,30 @@ function transposeMatrix(matrix) {
   document.getElementById('columns').addEventListener('change', function() {
     document.getElementById('choleskyButton').style.display = 'none';
 });
+
+// When the user loads the page
+window.onload = function() {
+  var modal = document.getElementById("popupModal");
+  var span = document.getElementsByClassName("close-button")[0];
+
+  // Display the modal
+  modal.style.display = "flex";
+  // selain block, bisa juga flex, inline, inline-block, none
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  //close button click = close modal
+  document.getElementById("close-button").onclick = function() {
+    modal.style.display = "none";
+  };
+};
